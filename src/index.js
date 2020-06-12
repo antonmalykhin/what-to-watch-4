38ae1eb5a0cb81bc31384fb845b2b674c9aa8c50
antonmalykhin <a.y.malykhin@gmail.com>
2020-06-12 14:18:59 +0300

    Добавляет список фильмов

--- src/index.js
+++ src/index.js
@@ -8,7 +8,18 @@ const Film = {
   RELEASE: 2014
 };
 
+const FILM_TITLES = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `No Country for Old Men`
+];
+
 ReactDOM.render(
-    <App title={Film.TITLE} genre={Film.GENRE} release={Film.RELEASE}/>,
+    <App title={Film.TITLE} genre={Film.GENRE} release={Film.RELEASE} filmTitles={FILM_TITLES}/>,
     document.querySelector(`#root`)
 );
