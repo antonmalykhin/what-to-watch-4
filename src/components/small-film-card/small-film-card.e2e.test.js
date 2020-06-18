18f36b4757845cfcfbe30aae9bafe15d372b458c
antonmalykhin <a.y.malykhin@gmail.com>
2020-06-18 18:39:16 +0300

    Добавляет e2e тест

---
+++ src/components/small-film-card/small-film-card.e2e.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import SmallFilmCard from './small-film-card.jsx';
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+const film = {
+  title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+};
+
+it(`Film data passed to callback`, () => {
+  const onCardMouseOver = jest.fn();
+
+  const smallFilmCard = shallow(
+      <SmallFilmCard
+        film={film}
+        onFilmTitleClick={() => {}}
+        onFilmMouseOver={onCardMouseOver}
+      />);
+
+  const filmCard = smallFilmCard.find(`.small-movie-card`);
+  filmCard.props().onMouseOver();
+
+  expect(onCardMouseOver).toHaveBeenCalledTimes(1);
+  expect(onCardMouseOver.mock.calls[0][0]).toMatchObject(film);
+});
