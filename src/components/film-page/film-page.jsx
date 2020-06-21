f5c3063e76692f770ef3865194b221cf77eb898e
antonmalykhin <a.y.malykhin@gmail.com>
2020-06-21 18:33:10 +0300

    Подключает FilmList, добавляет props, добавляет валидацию props

--- src/components/film-page/film-page.jsx
+++ src/components/film-page/film-page.jsx
@@ -1,4 +1,9 @@
 import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import FilmList from '../film-list/film-list.jsx';
+
+
+const MORE_LIKE_THIS_FILM_COUNT = 4;
 
 class FilmPage extends PureComponent {
   constructor(props) {
@@ -7,12 +12,18 @@ class FilmPage extends PureComponent {
   }
 
   render() {
+    const {film, films, onFilmTitleClick} = this.props;
+    const {background, title, genre, release, poster, rating, description, crew} = film;
+    const {score, level, count} = rating;
+    const {director, starring} = crew;
+    const moreLikeThisFilms = films.slice(0, MORE_LIKE_THIS_FILM_COUNT);
+
     return (
       <React.Fragment>
         <section className="movie-card movie-card--full">
           <div className="movie-card__hero">
             <div className="movie-card__bg">
-              <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+              <img src={background} alt={title} />
             </div>
 
             <h1 className="visually-hidden">WTW</h1>
@@ -35,10 +46,10 @@ class FilmPage extends PureComponent {
 
             <div className="movie-card__wrap">
               <div className="movie-card__desc">
-                <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+                <h2 className="movie-card__title">{title}</h2>
                 <p className="movie-card__meta">
-                  <span className="movie-card__genre">Drama</span>
-                  <span className="movie-card__year">2014</span>
+                  <span className="movie-card__genre">{genre}</span>
+                  <span className="movie-card__year">{release}</span>
                 </p>
 
                 <div className="movie-card__buttons">
@@ -63,7 +74,7 @@ class FilmPage extends PureComponent {
           <div className="movie-card__wrap movie-card__translate-top">
             <div className="movie-card__info">
               <div className="movie-card__poster movie-card__poster--big">
-                <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+                <img src={poster} alt={title} width="218" height="327" />
               </div>
 
               <div className="movie-card__desc">
@@ -82,21 +93,17 @@ class FilmPage extends PureComponent {
                 </nav>
 
                 <div className="movie-rating">
-                  <div className="movie-rating__score">8,9</div>
+                  <div className="movie-rating__score">{score}</div>
                   <p className="movie-rating__meta">
-                    <span className="movie-rating__level">Very good</span>
-                    <span className="movie-rating__count">240 ratings</span>
+                    <span className="movie-rating__level">{level}</span>
+                    <span className="movie-rating__count">{count} ratings</span>
                   </p>
                 </div>
 
                 <div className="movie-card__text">
-                  <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&apos;s friend and protege.</p>
-
-                  <p>Gustave prides himself on providing first-class service to the hotel&apos;s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave&apos;s lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
-
-                  <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-
-                  <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
+                  <p>{description}</p>
+                  <p className="movie-card__director"><strong>Director: {director}</strong></p>
+                  <p className="movie-card__starring"><strong>Starring: {starring}</strong></p>
                 </div>
               </div>
             </div>
@@ -107,43 +114,8 @@ class FilmPage extends PureComponent {
           <section className="catalog catalog--like-this">
             <h2 className="catalog__title">More like this</h2>
 
-            <div className="catalog__movies-list">
-              <article className="small-movie-card catalog__movies-card">
-                <div className="small-movie-card__image">
-                  <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
-                </div>
-                <h3 className="small-movie-card__title">
-                  <a className="small-movie-card__link" href="movie-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
-                </h3>
-              </article>
+            {<FilmList films={moreLikeThisFilms} onFilmTitleClick={onFilmTitleClick}/>}
 
-              <article className="small-movie-card catalog__movies-card">
-                <div className="small-movie-card__image">
-                  <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
-                </div>
-                <h3 className="small-movie-card__title">
-                  <a className="small-movie-card__link" href="movie-page.html">Bohemian Rhapsody</a>
-                </h3>
-              </article>
-
-              <article className="small-movie-card catalog__movies-card">
-                <div className="small-movie-card__image">
-                  <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
-                </div>
-                <h3 className="small-movie-card__title">
-                  <a className="small-movie-card__link" href="movie-page.html">Macbeth</a>
-                </h3>
-              </article>
-
-              <article className="small-movie-card catalog__movies-card">
-                <div className="small-movie-card__image">
-                  <img src="img/aviator.jpg" alt="Aviator" width="280" height="175" />
-                </div>
-                <h3 className="small-movie-card__title">
-                  <a className="small-movie-card__link" href="movie-page.html">Aviator</a>
-                </h3>
-              </article>
-            </div>
           </section>
 
           <footer className="page-footer">
@@ -164,5 +136,30 @@ class FilmPage extends PureComponent {
     );
   }
 }
+
+FilmPage.propTypes = {
+  film: PropTypes.shape({
+    background: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    release: PropTypes.number.isRequired,
+    poster: PropTypes.string.isRequired,
+    rating: PropTypes.shape({
+      score: PropTypes.number.isRequired,
+      level: PropTypes.string.isRequired,
+      count: PropTypes.number.isRequired
+    }).isRequired,
+    description: PropTypes.string.isRequired,
+    crew: PropTypes.shape({
+      director: PropTypes.string.isRequired,
+      starring: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired,
+  films: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired
+  })).isRequired,
+  onFilmTitleClick: PropTypes.func.isRequired
+};
 
 export default FilmPage;
