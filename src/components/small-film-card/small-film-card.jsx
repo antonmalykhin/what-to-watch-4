56073c3a87a1dfd635b709472be145b21affd1c5
antonmalykhin <a.y.malykhin@gmail.com>
2020-06-26 12:24:13 +0300

    Подключает FilmPlayer, реализует показ превью при наведении на карточку фильма

--- src/components/small-film-card/small-film-card.jsx
+++ src/components/small-film-card/small-film-card.jsx
@@ -1,21 +1,61 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import VideoPlayer from '../video-player/video-player.jsx';
+
+const TIMEOUT = 1000;
+
+const filmPreviewSettings = {
+  WIDTH: 280,
+  HEIGHT: 175,
+  MUTED: true,
+  LOOP: true
+};
 
 class SmallFilmCard extends PureComponent {
   constructor(props) {
     super(props);
 
+    this.timeoutID = null;
+
+    this.state = {
+      isPlaying: false
+    };
   }
 
   render() {
     const {film, onFilmClick, onFilmMouseOver} = this.props;
-    const {title, image} = film;
+    const {title, image, preview} = film;
 
     return (
-      <article className="small-movie-card catalog__movies-card" onMouseOver={() => onFilmMouseOver(film)}>
-        <div className="small-movie-card__image" onClick={() => onFilmClick(film)} >
-          <img src={image} alt={title} width="280" height="175" />
+      <article
+        className="small-movie-card catalog__movies-card"
+        onMouseOver={() => {
+          onFilmMouseOver(film);
+        }}
+      >
+
+        <div
+          className="small-movie-card__image"
+          onClick={() => onFilmClick(film)}
+          onMouseOver={() => {
+            this.timeoutID = setTimeout(() => this.setState({isPlaying: true}), TIMEOUT);
+          }}
+          onMouseOut={() => {
+            this.setState({isPlaying: false});
+            clearTimeout(this.timeoutID);
+          }}
+        >
+          <VideoPlayer
+            width={filmPreviewSettings.WIDTH}
+            height={filmPreviewSettings.HEIGHT}
+            src={preview}
+            poster={image}
+            muted={filmPreviewSettings.MUTED}
+            loop={filmPreviewSettings.LOOP}
+            isPlaying={this.state.isPlaying}
+          />
         </div>
+
         <h3 className="small-movie-card__title">
           <a className="small-movie-card__link" href="movie-page.html" onClick={(evt) => {
             evt.preventDefault();
@@ -30,7 +70,8 @@ class SmallFilmCard extends PureComponent {
 SmallFilmCard.propTypes = {
   film: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired
+    image: PropTypes.string.isRequired,
+    preview: PropTypes.string.isRequired,
   }).isRequired,
   onFilmMouseOver: PropTypes.func.isRequired,
   onFilmClick: PropTypes.func.isRequired
