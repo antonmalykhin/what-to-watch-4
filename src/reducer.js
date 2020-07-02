8f8bafb692a94b51c080a4325bf53f4dc1411944
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-02 13:22:39 +0300

    Добавляет reducer.js

---
+++ src/reducer.js
@@ -0,0 +1,34 @@
+const InitialState = {
+  genre: ``,
+  films: []
+};
+
+const ActionType = {
+  CHANGE_GENRE_FILTER: `CHANGE_GENRE_FILTER`,
+  FILTER_FILMS: `FILTER_FILMS`
+};
+
+export const changeGenreFilter = (genre) => {
+  return {
+    type: ActionType.CHANGE_GENRE_FILTER,
+    payload: genre
+  };
+};
+
+export const getFilteredFilms = (films) => {
+  return {
+    type: ActionType.FILTER_FILMS,
+    payload: films
+  };
+};
+
+export const reducer = (state = InitialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_GENRE_FILTER:
+      return Object.assign({}, state, {genre: action.payload});
+    case ActionType.FILTER_FILMS:
+      return Object.assign({}, state, {films: action.payload});
+  }
+  return state;
+};
+
