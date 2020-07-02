76345ecb0c454f7c98eb5f62cd54837db7083821
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-03 00:59:16 +0300

    Импортирует моки фильмов, добавляет объект ActionCreator

--- src/reducer.js
+++ src/reducer.js
@@ -1,26 +1,31 @@
+import films from './mock/films.js';
+
 const InitialState = {
   genre: `All genres`,
-  films: []
+  films
 };
 
-const ActionType = {
+export const ActionType = {
   CHANGE_GENRE_FILTER: `CHANGE_GENRE_FILTER`,
   FILTER_FILMS: `FILTER_FILMS`
 };
 
-export const changeGenreFilter = (genre) => {
-  return {
-    type: ActionType.CHANGE_GENRE_FILTER,
-    payload: genre
-  };
+export const ActionCreator = {
+  changeGenreFilter: (genre) => {
+    return {
+      type: ActionType.CHANGE_GENRE_FILTER,
+      payload: genre
+    };
+  },
+
+  getFilteredFilms: (filteredFilms) => {
+    return {
+      type: ActionType.FILTER_FILMS,
+      payload: filteredFilms
+    };
+  }
 };
 
-export const getFilteredFilms = (films) => {
-  return {
-    type: ActionType.FILTER_FILMS,
-    payload: films
-  };
-};
 
 export const reducer = (state = InitialState, action) => {
   switch (action.type) {
