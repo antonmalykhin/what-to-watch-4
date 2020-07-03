0a6600491c5d904b7db2edc90d6df7331fb35dbd
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-03 15:34:29 +0300

    Удаляет лишний коммент

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
 import {reducer} from './reducer.js';
-// import films from './mock/films.js';
 
 const store = createStore(
     reducer,
