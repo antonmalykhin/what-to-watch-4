1b0ae2f8740b673a003a2e56936269616efe8b00
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-03 15:04:49 +0300

    Добавляте стейт нажатия на карточку фильма

--- src/reducer.js
+++ src/reducer.js
@@ -2,11 +2,13 @@ import films from './mock/films.js';
 
 const InitialState = {
   genre: `All genres`,
+  currentFilm: {},
   films
 };
 
 export const ActionType = {
   CHANGE_GENRE_FILTER: `CHANGE_GENRE_FILTER`,
+  CHANGE_CURRENT_FILM: `CHANGE_CURRENT_FILM`,
   FILTER_FILMS: `FILTER_FILMS`
 };
 
@@ -15,6 +17,13 @@ export const ActionCreator = {
     return {
       type: ActionType.CHANGE_GENRE_FILTER,
       payload: genre
+    };
+  },
+
+  changeCurrentFilm: (film) => {
+    return {
+      type: ActionType.CHANGE_CURRENT_FILM,
+      payload: film
     };
   },
 
@@ -31,6 +40,8 @@ export const reducer = (state = InitialState, action) => {
   switch (action.type) {
     case ActionType.CHANGE_GENRE_FILTER:
       return Object.assign({}, state, {genre: action.payload});
+    case ActionType.CHANGE_CURRENT_FILM:
+      return Object.assign({}, state, {currentFilm: action.payload});
     case ActionType.FILTER_FILMS:
       return Object.assign({}, state, {films: action.payload});
   }
