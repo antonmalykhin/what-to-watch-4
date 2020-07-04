cd952899d6708e3a77cd7878d89f73f9eca67def
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-04 18:26:12 +0300

    Добавляет  showedFilms в стейт, добвляет экшны увеличения кол-ва показаных фильмов и экшн сброса

--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,19 @@
 import films from './mock/films.js';
 import {extend} from './utils.js';
 
+const FILM_COUNT_ON_START = 8;
+const INCREMENT_FILM_COUNT = 8;
 const InitialState = {
   genre: `All genres`,
+  showedFilms: FILM_COUNT_ON_START,
   currentFilm: {},
   films
 };
 
 export const ActionType = {
   CHANGE_GENRE_FILTER: `CHANGE_GENRE_FILTER`,
+  INCREMENT_SHOWED_FILM_COUNT: `INCREMENT_SHOWED_FILM_COUNT`,
+  RESET_SHOWED_FILM_COUNT: `RESET_SHOWED_FILM_COUNT`,
   CHANGE_CURRENT_FILM: `CHANGE_CURRENT_FILM`,
   FILTER_FILMS: `FILTER_FILMS`
 };
@@ -18,6 +23,20 @@ export const ActionCreator = {
     return {
       type: ActionType.CHANGE_GENRE_FILTER,
       payload: genre
+    };
+  },
+
+  incrementShowedFilmCount: () => {
+    return {
+      type: ActionType.INCREMENT_SHOWED_FILM_COUNT,
+      payload: INCREMENT_FILM_COUNT
+    };
+  },
+
+  resetShowedFilmCount: () => {
+    return {
+      type: ActionType.RESET_SHOWED_FILM_COUNT,
+      payload: FILM_COUNT_ON_START
     };
   },
 
@@ -42,6 +61,14 @@ export const reducer = (state = InitialState, action) => {
     case ActionType.CHANGE_GENRE_FILTER:
       return extend(state, {
         genre: action.payload
+      });
+    case ActionType.INCREMENT_SHOWED_FILM_COUNT:
+      return extend(state, {
+        showedFilms: state.showedFilms + action.payload
+      });
+    case ActionType.RESET_SHOWED_FILM_COUNT:
+      return extend(state, {
+        showedFilms: action.payload
       });
     case ActionType.CHANGE_CURRENT_FILM:
       return extend(state, {
