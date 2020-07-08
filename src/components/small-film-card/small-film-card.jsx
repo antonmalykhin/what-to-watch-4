ec4eb764a42d6ea53abf56833eb3c123deefbaa0
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-09 02:25:17 +0300

    Выносит состояние в hoc

--- src/components/small-film-card/small-film-card.jsx
+++ src/components/small-film-card/small-film-card.jsx
@@ -1,8 +1,7 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import VideoPlayer from '../video-player/video-player.jsx';
-
-const TIMEOUT = 1000;
+import withActivePlayer from '../../hocks/with-active-player/with-active-player.js';
 
 const filmPreviewSettings = {
   WIDTH: 280,
@@ -10,70 +9,47 @@ const filmPreviewSettings = {
   LOOP: true
 };
 
-class SmallFilmCard extends PureComponent {
-  constructor(props) {
-    super(props);
+const SmallFilmCard = (props) => {
+  const {film, onFilmClick, onFilmMouseOver, isPlaying, onPlayVideo, onStopVideo} = props;
+  const {title, image, preview} = film;
 
-    this._timeoutID = null;
+  return (
+    <article
+      className="small-movie-card catalog__movies-card"
+      onMouseOver={() => {
+        onFilmMouseOver(film);
+      }}
+    >
 
-    this.state = {
-      isPlaying: false
-    };
-  }
-
-  componentWillUnmount() {
-    if (this._timeoutID) {
-      clearTimeout(this._timeoutID);
-    }
-  }
-
-  render() {
-    const {film, onFilmClick, onFilmMouseOver} = this.props;
-    const {title, image, preview} = film;
-
-    return (
-      <article
-        className="small-movie-card catalog__movies-card"
-        onMouseOver={() => {
-          onFilmMouseOver(film);
-        }}
+      <div
+        className="small-movie-card__image"
+        onClick={() => onFilmClick(film)}
+        onMouseOver={() => onPlayVideo()}
+        onMouseOut={() => onStopVideo()}
       >
+        <VideoPlayer
+          width={filmPreviewSettings.WIDTH}
+          height={filmPreviewSettings.HEIGHT}
+          src={preview}
+          poster={image}
+          loop={filmPreviewSettings.LOOP}
+          isPlaying={isPlaying}
+        />
+        <img src={image} alt={title} width="280" height="175" />
+      </div>
 
-        <div
-          className="small-movie-card__image"
-          onClick={() => onFilmClick(film)}
-          onMouseOver={() => {
-            this._timeoutID = setTimeout(() => this.setState({isPlaying: true}), TIMEOUT);
-          }}
-          onMouseOut={() => {
-            this.setState({isPlaying: false});
-            clearTimeout(this._timeoutID);
-          }}
-        >
-          <VideoPlayer
-            width={filmPreviewSettings.WIDTH}
-            height={filmPreviewSettings.HEIGHT}
-            src={preview}
-            poster={image}
-            loop={filmPreviewSettings.LOOP}
-            isPlaying={this.state.isPlaying}
-          />
-          <img src={image} alt={title} width="280" height="175"/>
-        </div>
-
-        <h3 className="small-movie-card__title">
-          <a className="small-movie-card__link" href="movie-page.html"
-            onClick={(evt) => {
-              evt.preventDefault();
-              onFilmClick(film);
-            }}>
-            {title}
-          </a>
-        </h3>
-      </article>
-    );
-  }
-}
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html"
+          onClick={(evt) => {
+            evt.preventDefault();
+            onFilmClick(film);
+          }}>
+          {title}
+        </a>
+      </h3>
+    </article>
+  );
+};
 
 SmallFilmCard.propTypes = {
   film: PropTypes.shape({
@@ -82,7 +58,10 @@ SmallFilmCard.propTypes = {
     preview: PropTypes.string.isRequired,
   }).isRequired,
   onFilmMouseOver: PropTypes.func.isRequired,
-  onFilmClick: PropTypes.func.isRequired
+  onFilmClick: PropTypes.func.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+  onPlayVideo: PropTypes.func.isRequired,
+  onStopVideo: PropTypes.func.isRequired
 };
 
-export default SmallFilmCard;
+export default withActivePlayer(SmallFilmCard);
