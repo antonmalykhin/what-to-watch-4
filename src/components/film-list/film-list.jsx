0534b8fd33c34c76c17fb5d3585626adfec94e0c
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-09 02:42:28 +0300

    Преносит состояние в hoc

--- src/components/film-list/film-list.jsx
+++ src/components/film-list/film-list.jsx
@@ -1,43 +1,36 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import SmallFilmCard from '../small-film-card/small-film-card.jsx';
+import withActiveFilm from '../../hocks/with-active-film/with-active-film.js';
 
-class FilmList extends PureComponent {
-  constructor(props) {
-    super(props);
+const FilmList = (props) => {
+  const {films, onFilmClick, onActiveFilmChange} = props;
 
-    this.state = {
-      isActive: this.props.films[0]
-    };
-  }
+  return (
+    <div className="catalog__movies-list">
+      {films.map((film, index) => (
+        <SmallFilmCard
+          key={`${index}-${film.title}`}
+          onFilmClick={onFilmClick}
+          onFilmMouseOver={(filmData) => {
+            onActiveFilmChange(filmData);
+          }}
+          film={film} />
+      ))}
+    </div>
+  );
+};
 
-  render() {
-    const {films, onFilmClick} = this.props;
-
-    return (
-      <div className="catalog__movies-list">
-        {films.map((film, index) => (
-          <SmallFilmCard
-            key={`${index}-${film.title}`}
-            onFilmClick={onFilmClick}
-            onFilmMouseOver={(filmData) => {
-              this.setState({isActive: filmData});
-            }}
-            film={film} />
-        ))}
-      </div>
-    );
-  }
-}
 
 FilmList.propTypes = {
   films: PropTypes.arrayOf(PropTypes.shape({
     title: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired
   })).isRequired,
-  onFilmClick: PropTypes.func.isRequired
+  onFilmClick: PropTypes.func.isRequired,
+  onActiveFilmChange: PropTypes.func.isRequired
 };
 
-export default FilmList;
+export default withActiveFilm(FilmList);
 
 
