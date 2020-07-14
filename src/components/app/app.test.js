b8b7a59cebfc7b4103664cc11d42a632027931d4
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-14 15:23:13 +0300

    Исправялет ошибку

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
-import { App } from './app.jsx';
+import {App} from './app.jsx';
 
 const mockStore = configureStore([]);
 
