08f89cd00ada32c8ecefcb4500b28250991ef68e
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-16 22:58:21 +0300

    Реализует функциональность контролов, показ оставшегося времени, добавляет возможность закрытия плеера

--- src/components/main-video-player/main-video-player.jsx
+++ src/components/main-video-player/main-video-player.jsx
@@ -1,6 +1,6 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import {convertMinutesToSeconds, formatTime} from '../../utils.js';
+import {formatTime} from '../../utils.js';
 
 
 class MainVideoPlayer extends PureComponent {
@@ -10,33 +10,86 @@ class MainVideoPlayer extends PureComponent {
     this._videoRef = React.createRef();
 
     this.state = {
-      isPlaying: false
+      duration: 0,
+      progress: 0,
+      isPlaying: false,
     };
 
     this._handlePlay = this._handlePlay.bind(this);
-    this._handleStop = this._handleStop.bind(this);
+  }
 
+  componentDidMount() {
+    const film = this._videoRef.current;
+    const {video} = this.props.film;
+
+    film.src = video;
+
+    // film.oncanplaythrough = () => this.setState({
+    //   isPlaying: true
+    // });
+
+    film.onloadedmetadata = () => this.setState({
+      duration: Math.floor(film.duration)
+    });
+
+    film.onplay = () => this.setState({
+      isPlaying: true
+    });
+
+    film.onpause = () => this.setState({
+      isPlaying: false
+    });
+
+    film.ontimeupdate = () => this.setState({
+      progress: Math.floor(film.currentTime)
+    });
+
+  }
+
+  componentWillUnmount() {
+    const film = this._videoRef.current;
+
+    film.oncanplaythrough = null;
+    film.onplay = null;
+    film.onpause = null;
+    film.ontimeupdate = null;
+    film.src = ``;
+  }
+
+  componentDidUpdate() {
+    const film = this._videoRef.current;
+
+    if (this.state.isPlaying) {
+      film.play();
+    } else {
+      film.pause();
+    }
   }
 
   _handlePlay() {
-    return this.setState({isPlaying: true});
-  }
-
-  _handleStop() {
-    return this.setState({isPlaying: false});
+    return this.setState({isPlaying: !this.state.isPlaying});
   }
 
   render() {
-    const {film} = this.props;
+    const {
+      film,
+      onExitClick
+    } = this.props;
 
     const {
       title,
-      runtime,
       video
     } = film;
 
-    let remainingTime = convertMinutesToSeconds(runtime);
-    let formattedRemainingTime = formatTime(remainingTime);
+    const {
+      duration,
+      progress,
+      isPlaying,
+    } = this.state;
+
+    const remainingTime = duration - progress;
+    const formattedRemainingTime = formatTime(remainingTime);
+    const togglerPosition = progress * 100 / duration;
 
     return (
       <div className="player">
@@ -50,15 +103,15 @@ class MainVideoPlayer extends PureComponent {
           type="button"
           className="player__exit"
           onClick={() => {
-            console.log(`exit`);
+            onExitClick();
           }}
         >Exit</button>
 
         <div className="player__controls">
           <div className="player__controls-row">
             <div className="player__time">
-              <progress className="player__progress" value="30" max="100"></progress>
-              <div className="player__toggler" style={{ left: `30%` }}>Toggler</div>
+              <progress className="player__progress" value={progress} max={duration}></progress>
+              <div className="player__toggler" style={{left: `${togglerPosition}%`}}>Toggler</div>
             </div>
             <div className="player__time-value">{formattedRemainingTime}</div>
           </div>
@@ -68,11 +121,11 @@ class MainVideoPlayer extends PureComponent {
               type="button"
               className="player__play"
               onClick={() => {
-                console.log(`play`);
+                this._handlePlay();
               }}
             >
               <svg viewBox="0 0 19 19" width="19" height="19">
-                <use xlinkHref="#play-s"></use>
+                <use xlinkHref={`#${isPlaying ? `pause` : `play-s`}`}></use>
               </svg>
               <span>Play</span>
             </button>
@@ -81,9 +134,7 @@ class MainVideoPlayer extends PureComponent {
             <button
               type="button"
               className="player__full-screen"
-              onClick={() => {
-                console.log(`fullscreen`);
-              }}
+              onClick={() => this._videoRef.current.requestFullscreen()}
             >
               <svg viewBox="0 0 27 27" width="27" height="27">
                 <use xlinkHref="#full-screen"></use>
@@ -100,9 +151,9 @@ class MainVideoPlayer extends PureComponent {
 MainVideoPlayer.propTypes = {
   film: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    runtime: PropTypes.number.isRequired,
     video: PropTypes.string.isRequired
-  }).isRequired
+  }).isRequired,
+  onExitClick: PropTypes.func.isRequired
 };
 
 export default MainVideoPlayer;
