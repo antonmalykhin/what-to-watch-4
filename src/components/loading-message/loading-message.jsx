7d62ea90103e2e5a4842baf63ca997ebb1283b65
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-21 00:24:52 +0300

    Добавляет LoadingMessage

---
+++ src/components/loading-message/loading-message.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Style = {
+  color: `#eee5b5`,
+  fontWeight: `bold`,
+  textAlign: `center`
+};
+
+const LoadingMessage = () => {
+  return <p style={Style}>Loading...</p>;
+};
+export default LoadingMessage;
