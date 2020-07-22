a0684e1cef85326863adaf0566c062993d7f6d46
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-23 02:42:15 +0300

    Добавляет компонент SignIn, добаляет тест

---
+++ src/components/sign-in/sign-in.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SignIn from './sign-in';
+
+
+it(`SignIn render correctly`, () => {
+  const tree = renderer.create(
+      <SignIn
+        onSubmit={() => {}}
+      />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
