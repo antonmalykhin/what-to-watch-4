0a71559597bcda47668980dfe8f0bb374b80fbf2
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-24 00:47:49 +0300

    Подключает AddReview

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,6 +6,7 @@ import {isEmptyObject} from '../../utils.js';
 import Main from '../main/main.jsx';
 import FilmPage from '../film-page/film-page.jsx';
 import SignIn from '../sign-in/sign-in.jsx';
+import AddReview from '../add-review/add-review.jsx';
 import MainVideoPlayer from '../main-video-player/main-video-player.jsx';
 import withActiveMainPlayer from '../../hocks/with-active-main-player/with-active-main-player.js';
 import {ActionCreator as AppActionCreator} from '../../reducer/app/app.js';
@@ -35,6 +36,7 @@ class App extends PureComponent {
       onExitButtonClick,
       login
     } = this.props;
+
     if (authorizationStatus === AuthorizationStatus.AUTH) {
       if (isEmptyObject(currentFilm) && isEmptyObject(playingFilm)) {
         return (
@@ -99,6 +101,9 @@ class App extends PureComponent {
           </Route>
           <Route exact path="/dev-sign-in">
             <SignIn onSubmit={login}/>
+          </Route>
+          <Route exact path="/dev-add-review">
+            <AddReview filmID={42}/>
           </Route>
         </Switch>
       </BrowserRouter>
