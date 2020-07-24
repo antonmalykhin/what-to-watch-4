14e4096b876b5eeadcf2d78c70fad6bd5c392904
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-25 01:07:47 +0300

    Обновляет тесты

---
+++ src/components/add-review/add-review.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import AddReview from './add-review.jsx';
+
+it(`AddReview render correctly`, () => {
+  const tree = renderer.create(
+      <AddReview
+        filmID={13}
+        onSubmit={() => {}}
+        rating={3}
+        onRatingCheck={() => {}}
+      />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
