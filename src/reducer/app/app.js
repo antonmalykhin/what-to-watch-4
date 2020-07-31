ad9dc57877d8c0a7466bd459109fc4cd50be1932
antonmalykhin <a.y.malykhin@gmail.com>
2020-08-01 01:09:54 +0300

    Удаляет лишнее

--- src/reducer/app/app.js
+++ src/reducer/app/app.js
@@ -6,15 +6,11 @@ const INCREMENT_FILM_COUNT = 8;
 const InitialState = {
   currentYear: 0,
   showedFilms: FILM_COUNT_ON_START,
-  playingFilm: {},
 };
 
 export const ActionType = {
   INCREMENT_SHOWED_FILM_COUNT: `INCREMENT_SHOWED_FILM_COUNT`,
   RESET_SHOWED_FILM_COUNT: `RESET_SHOWED_FILM_COUNT`,
-  FILTER_FILMS: `FILTER_FILMS`,
-  OPEN_MAIN_PLAYER: `OPEN_MAIN_PLAYER`,
-  CLOSE_MAIN_PLAYER: `CLOSE_MAIN_PLAYER`,
   GET_CURRENT_YEAR: `GET_CURRENT_YEAR`
 };
 
@@ -30,27 +26,6 @@ export const ActionCreator = {
     return {
       type: ActionType.RESET_SHOWED_FILM_COUNT,
       payload: FILM_COUNT_ON_START
-    };
-  },
-
-  getFilteredFilms: (filteredFilms) => {
-    return {
-      type: ActionType.FILTER_FILMS,
-      payload: filteredFilms
-    };
-  },
-
-  openMainPlayer: (film) => {
-    return {
-      type: ActionType.OPEN_MAIN_PLAYER,
-      payload: film
-    };
-  },
-
-  closeMainPlayer: () => {
-    return {
-      type: ActionType.CLOSE_MAIN_PLAYER,
-      payload: {}
     };
   },
 
@@ -71,18 +46,6 @@ export const reducer = (state = InitialState, action) => {
     case ActionType.RESET_SHOWED_FILM_COUNT:
       return extend(state, {
         showedFilms: action.payload
-      });
-    case ActionType.FILTER_FILMS:
-      return extend(state, {
-        films: action.payload
-      });
-    case ActionType.OPEN_MAIN_PLAYER:
-      return extend(state, {
-        playingFilm: action.payload
-      });
-    case ActionType.CLOSE_MAIN_PLAYER:
-      return extend(state, {
-        playingFilm: action.payload
       });
     case ActionType.GET_CURRENT_YEAR:
       return extend(state, {
